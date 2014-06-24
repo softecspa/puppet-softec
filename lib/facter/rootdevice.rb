b8ce1012aec0857061ba710509f2c4300401a8c2
root on puppetmaster <root@puppetmaster.vagrant.local>
2014-06-24 10:24:12 +0000

    deleted rootdevice not yet used

--- lib/facter/rootdevice.rb
+++
@@ -1,8 +0,0 @@
-# Calculates the device file for the root filesystem
-require 'facter'
-
-Facter.add("rootdevice") do
-	setcode do
-		%x{/bin/mount | egrep 'on\ \/\ ' | awk '{ print $1 }'}.chomp
-	end
-end
